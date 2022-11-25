1f79382f822ab270f13ebe648e4feeb4000f4b30
Ivan Andrade <ivanandrade611@gmail.com>
2022-11-24 20:08:15 -0500

    :up: :sparkles: Add validations request

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,12 @@
 import Router from "express";
 import controller from "../controllers/users.js";
+import { validateReqCreateUser } from "../validators/users.js"
+
 
 const routerUsers = Router();
 routerUsers.get("/", controller.list);
 routerUsers.get("/:id", controller.get);
-routerUsers.post("/create", controller.create);
+routerUsers.post("/create", validateReqCreateUser, controller.create);
 routerUsers.put("/:id", controller.update);
 routerUsers.delete("/:id", controller.delete);
 routerUsers.post("/changePassword", controller.changePassword);
